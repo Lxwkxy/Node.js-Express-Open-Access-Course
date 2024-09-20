62823258e16a478a49f7b2e7d793db5e94e7ecde
Lxwkxy <paingaikrab1234@gmail.com>
2024-09-21 01:29:09 +0700

    Add all files of Project
    Hello World :100:

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import chalk from 'chalk';
+// const express = require('express');
+// const chalk = require('chalk')
+import createDebug from 'debug'
+const debug = createDebug('app')
+// const debug = require('debug')('app');
+import morgan from 'morgan';
+// const morgan = require('morgan')
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.get("/", (req,res) =>{
+    res.send('Hello borntoDev Co., Ltd.');
+})
+
+app.listen(port, ()=>{
+    debug("Listening on port " + chalk.green(port));
+})
